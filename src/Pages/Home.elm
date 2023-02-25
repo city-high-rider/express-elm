57f219c00275f477607f96cc424e929e07d15917
nikolay <nikolay@localhost.localdomain>
2023-02-25 13:37:43 +1300

    create basic home page

---
+++ src/Pages/Home.elm
@@ -0,0 +1,11 @@
+module Pages.Home exposing (view)
+import Html exposing (text, div, Html, p, h2, a)
+import Html.Attributes exposing (href)
+
+view : Html msg
+view =
+    div []
+    [ h2 [] [text "Welcome to the website!"]
+    , p [] [text "This is a test to try and read and display some information from a database, using an expressjs backend."]
+    , a [href "/menu"] [text "go take a look at the menu!"]
+    ]
