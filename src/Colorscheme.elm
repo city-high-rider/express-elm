e5b56e295b3328b5018d87aac5de8ce31f5a7760
nikolay <nikolay@localhost.localdomain>
2023-03-21 22:30:09 +1300

    add new color to colorscheme

--- src/Colorscheme.elm
+++ src/Colorscheme.elm
@@ -7,6 +7,7 @@ type alias Colorscheme =
     { primary : Color
     , secondary : Color
     , fg : Color
+    , fgDarker : Color
     , bg : Color
     , misc : Color
     }
@@ -18,5 +19,6 @@ light =
         (rgb255 236 102 109)
         (rgb255 254 119 29)
         (rgb255 246 233 199)
+        (rgb255 206 193 159)
         (rgb255 116 113 123)
         (rgb255 71 152 136)
